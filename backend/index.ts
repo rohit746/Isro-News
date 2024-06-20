5607a376a6ee1d3d362486e2514cc8cb7bd78f7f
Rohit Jadhav <45796556+rohit746@users.noreply.github.com>
2024-06-20 11:12:21 +0530

    feat: id

--- backend/index.ts
+++ backend/index.ts
@@ -15,20 +15,21 @@ app.get("/api/press-releases", async (c) => {
 
     const $ = cheerio.load(html);
     const pressReleases: {
+      id: number;
       title: string;
       date: string;
       link: string;
     }[] = [];
 
-    $("table tbody tr").each((_index, element) => {
+    $("table tbody tr").each((index, element) => {
       const titleElement = $(element).find("td a");
       const titleText = titleElement.text().trim().replace(/\n\s*/g, " ");
       const rawDate = $(element).find("td").last().text().trim();
       const href = titleElement.attr("href");
 
       const dateObject = new Date(rawDate);
-      const day = dateObject.getDate().toString().padStart(2, "0"); // Get day as two digits
-      const month = (dateObject.getMonth() + 1).toString().padStart(2, "0"); // Get month as two digits (months are zero-based)
+      const day = dateObject.getDate().toString().padStart(2, "0");
+      const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
       const year = dateObject.getFullYear();
 
       const formattedDate = `${day}-${month}-${year}`;
@@ -36,6 +37,7 @@ app.get("/api/press-releases", async (c) => {
       if (!isNaN(dateObject.getTime()) && titleText && href) {
         const link = new URL(href, "https://www.isro.gov.in/").toString();
         pressReleases.push({
+          id: index + 1,
           title: titleText,
           date: formattedDate,
           link,
